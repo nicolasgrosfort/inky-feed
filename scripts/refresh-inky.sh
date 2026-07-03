d2c2632affba9e481128e6e19dd0684413d7ad78
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-07-03 14:46:26 +0200

    feat: update script

--- scripts/refresh-inky.sh
+++ scripts/refresh-inky.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
+
+# Attendre que le réseau soit disponible
+until ping -c1 github.com >/dev/null 2>&1; do
+    sleep 1
+done
+
+cd /home/pi-inky-feed/inky-feed || exit 1
+git pull
+
 source /home/pi-inky-feed/.virtualenvs/pimoroni/bin/activate
+
 wget -O /tmp/inky-image.jpg https://lab.tekh.studio/inky-feed/
 python /home/pi-inky-feed/Pimoroni/inky/examples/spectra6/image.py --file /tmp/inky-image.jpg
+
 sleep 5m
-# sudo shutdown -h now
+
+sudo shutdown -h now
