43291667d6a8bd39d5eb3fc101f07ba788b01331
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2026-07-04 18:01:33 +0200

    script: SLEEP_TIME

--- scripts/refresh-inky.sh
+++ scripts/refresh-inky.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+ENV_FILE="$(dirname "$0")/../.env"
+if [ -f "$ENV_FILE" ]; then
+  set -a
+  source "$ENV_FILE"
+  set +a
+fi
+
+SLEEP_TIME="${SLEEP_TIME:-300}"
+
 # Attendre que le réseau soit disponible
 until ping -c1 github.com >/dev/null 2>&1; do
     sleep 1
@@ -13,6 +22,6 @@ source /home/pi-inky-feed/.virtualenvs/pimoroni/bin/activate
 wget -O /tmp/inky-image.jpg https://lab.tekh.studio/inky-feed/
 python /home/pi-inky-feed/Pimoroni/inky/examples/spectra6/image.py --file /tmp/inky-image.jpg
 
-sleep 5m
+sleep "$SLEEP_TIME"
 
  sudo shutdown -h now
